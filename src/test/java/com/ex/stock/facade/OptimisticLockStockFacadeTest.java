15b898e764cac74e223030c8ede54d3343181f72
손지희 <wlhee@ejbt.co.kr>
2024-04-12 15:22:37 +0900

    feat: Optimistic Lock 서비스 추가

---
+++ src/test/java/com/ex/stock/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,67 @@
+package com.ex.stock.facade;
+
+import com.ex.stock.domain.Stock;
+import com.ex.stock.repository.StockRepository;
+import com.ex.stock.service.PessimisticLockStockService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class OptimisticLockStockFacadeTest {
+
+    @Autowired
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개의요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    optimisticLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findByIdWithOptimisticLock(1L);
+
+        Optional<Long> expected = Optional.of(0L);
+        Optional<Long> actual = Optional.of(stock.getQuantity());
+
+        // Optional 객체 내의 값을 비교
+        Assert.assertEquals(expected, actual);
+    }
+}
